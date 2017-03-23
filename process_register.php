b6b1b6e0deed3a4eebca63c18287a82d3645bb20
Anthony Mai <s3550974@student.rmit.edu.au>
2017-03-23 13:39:07 +1100

    data validation working for register, validation output is ugly though

--- process_register.php
+++ process_register.php
@@ -1,4 +1,5 @@
 <?php
+	/* Post */
 	if(isset($_POST['FName']))
         $FName = $_POST['FName'];
 	if(isset($_POST['LName']))
@@ -7,8 +8,29 @@
         $Email = $_POST['UName'];
 	if(isset($_POST['Password']))
         $Password = $_POST['Password'];
-	
+	if(isset($_POST['CPassword']))
+        $CPassword = $_POST['CPassword'];
+
+	/* Data validation */
+	/* current method of outputting information of incorrect data
+	 * is a little rudimentary, will look into better methods.
+	 */
+	/* password check */
+	if($Password != $CPassword) 
+		die('Passwords do not match');
+	/* email validation */
+	if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+		die("Invalid email format");
+	}
+	/* email duplicate validation */
 	$db = mysqli_connect("localhost", "root","", "bookingsystem")  or die(mysqli_error($db));
+	$sql = "select * from login where email = '$Email'";
+	$result = mysqli_query($db, $sql);
+	if(mysqli_num_rows($result)>=1){
+		die("Email is already in use.");
+	}
+
+	/* Database input */
 	$q = "insert into login values('$FName', '$LName', '$Email', '$Password')";
 	mysqli_query($db, $q) or die(mysqli_error($db));
 	
