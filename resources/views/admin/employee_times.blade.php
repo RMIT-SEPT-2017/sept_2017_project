5417f27086456c1404114f78a6cb649a7d50c9aa
Elliot Jackson <s3382313@student.rmit.edu.au>
2017-04-09 19:31:32 +1000

    Edit add availability page

--- resources/views/admin/employee_times.blade.php
+++ resources/views/admin/employee_times.blade.php
@@ -10,51 +10,51 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> <!-- load jquery via CDN -->
 <script type="text/javascript">
 
-	var d = new Date();
-	d.setDate(d.getDate() + (1 + 7 - d.getDay()) % 7);
+//	var d = new Date();
+//	d.setDate(d.getDate() + (1 + 7 - d.getDay()) % 7);
 	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 	var daysLabel = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 	var daysForm = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-function setDateFormat(){
-	
-	var d2 = d;
-	d2.setDate(d2.getDate() - 1);
-	for(var i=0;i<5;i++){
-		d2.setDate(d2.getDate() + 1);
-		var dd = d2.getDate();
-		var mm = d2.getMonth()+1; //January is 0!
-		var yyyy = d2.getFullYear();
-
-		if(dd<10) {
-		    dd='0'+dd
-		} 
-
-		if(mm<10) {
-		    mm='0'+mm
-		} 
-
-		var date = dd+'/'+mm+'/'+yyyy;
-		var phpDate = yyyy+'-'+mm+'-'+dd;
-		document.getElementById(daysLabel[d2.getDay()]).innerHTML = days[d2.getDay()] + ' '+(date);
-		document.getElementById(daysForm[d2.getDay()]).value = phpDate;
-	}
-}
-
-function nextWeek(){
-	d.setDate(d.getDate() + 7);
-	d.setDate(d.getDate() + (1 - d.getDay()) % 7);
-	setDateFormat();
-}
-function prevWeek(){
-	d.setDate(d.getDate() - 7);
-	d.setDate(d.getDate() + (1 - d.getDay()) % 7);
-	setDateFormat();
-}
-
-
+//function setDateFormat(){
+//	
+//	var d2 = d;
+//	d2.setDate(d2.getDate() - 1);
+//	for(var i=0;i<5;i++){
+//		d2.setDate(d2.getDate() + 1);
+//		var dd = d2.getDate();
+//		var mm = d2.getMonth()+1; //January is 0!
+//		var yyyy = d2.getFullYear();
+//
+//		if(dd<10) {
+//		    dd='0'+dd
+//		} 
+//
+//		if(mm<10) {
+//		    mm='0'+mm
+//		} 
+//
+//		var date = dd+'/'+mm+'/'+yyyy;
+//		var phpDate = yyyy+'-'+mm+'-'+dd;
+//		document.getElementById(daysLabel[d2.getDay()]).innerHTML = days[d2.getDay()] + ' '+(date);
+//		document.getElementById(daysForm[d2.getDay()]).value = phpDate;
+//	}
+//}
+//
+//function nextWeek(){
+//	d.setDate(d.getDate() + 7);
+//	d.setDate(d.getDate() + (1 - d.getDay()) % 7);
+//	setDateFormat();
+//}
+//function prevWeek(){
+//	d.setDate(d.getDate() - 7);
+//	d.setDate(d.getDate() + (1 - d.getDay()) % 7);
+//	setDateFormat();
+//}
 
 
-window.onload = setDateFormat;
+
+
+//window.onload = setDateFormat;
 
 </script>
 
@@ -69,36 +69,40 @@ window.onload = setDateFormat;
 
             <form id="day_form" name="day_form" action="{{ action('EmployeeTimesController@updateEmployeeTimes') }}" method="POST" class="ajax">
 
-            <label id="monday"></label>
-                <input type="hidden" id="mon" name="date1" value="" />
+            <label id="monday">Monday</label>
+                <input type="hidden" id="mon" name="day1" value="Monday" />
                 <input placeholder="Start Time" name="start1" type="time" value="09:00">
                 <input placeholder="End Time" name="end1" type="time" value="17:00">
 
-            <label id="tuesday"></label><br>
-                <input type="hidden" id="tue" name="date2" value="" />
+            <label id="tuesday">Tuesday</label><br>
+                <input type="hidden" id="tue" name="day2" value="Tuesday" />
                 <input placeholder="Start Time" name="start2" type="time" value="09:00">
                 <input placeholder="End Time" name="end2" type="time" value="17:00">
 
-            <label id="wednesday"></label><br>
-                <input type="hidden" id="wed" name="date3" value="" />
+            <label id="wednesday">Wednesday</label><br>
+                <input type="hidden" id="wed" name="day3" value="Wednesday" />
                 <input placeholder="Start Time" name="start3" type="time" value="09:00">
                 <input placeholder="End Time" name="end3" type="time" value="17:00">
 
-            <label id="thursday"></label><br>
-                <input type="hidden" id="thu" name="date4" value="" />
+            <label id="thursday">Thursday</label><br>
+                <input type="hidden" id="thu" name="day4" value="Thursday" />
                 <input placeholder="Start Time" name="start4" type="time" value="09:00">
                 <input placeholder="End Time" name="end4" type="time" value="17:00">
 
-            <label id="friday"></label><br>
-                <input type="hidden" id="fri" name="date5" value="" />
+            <label id="friday">Friday</label><br>
+                <input type="hidden" id="fri" name="day5" value="Friday" />
                 <input placeholder="Start Time" name="start5" type="time" value="09:00">
                 <input placeholder="End Time" name="end5" type="time" value="17:00"><br>
 
-                    <a><input type="change1" onclick="prevWeek()" value="Previous Week">
-                    <input type="change1" onclick="nextWeek()" value="Next Week"></a>
-                    <div class="sub">
+<!--
+            <a><input type="change1" onclick="prevWeek()" value="Previous Week">
+                <input type="change1" onclick="nextWeek()" value="Next Week"></a>
+-->
+                    
+                <div class="sub">
                         <input placeholder="Employee Id (we will create a dropdown menu from DB, ADD TO BACKLOG)" name="id" type="text">
                         <input type="submit" value="Submit"></a>
+                        
                     </div>
             </form>
         </div>
@@ -124,8 +128,6 @@ $('form.ajax').on('submit',function(){
             console.log(response);
         }
     });
-
-
 
 	return false;
 });
